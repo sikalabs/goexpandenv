9e86fab29bc7c611c05daf50ea487bf176a880c8
Ondrej Sika <ondrej@ondrejsika.com>
2024-01-26 11:20:52 +0100

    feat(install.sh): Add install script (inspired by slu install.sh)

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+VERSION=v0.1.0 && \
+OS=linux && \
+ARCH=amd64 && \
+BIN=goexpandenv && \
+curl -fsSL https://github.com/sikalabs/${BIN}/releases/download/${VERSION}/${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz -o ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
+tar -xzf ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz ${BIN} && \
+rm ${BIN}_${VERSION}_${OS}_${ARCH}.tar.gz && \
+mv ${BIN} /usr/local/bin/
